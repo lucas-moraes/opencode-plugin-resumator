6cb4102569b4a6aa4d487c275fcd198baf13e720
Lucas Moraes <lucas.moraes88@yahoo.com>
2026-08-23 14:48:19 -0300

    Split testable helpers into index.internal.js; thin index.js entrypoint so OpenCode only loads the plugin; bump to 0.5.5

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ import fs from "fs";
 import path from "path";
 import os from "os";
 
-const pluginModule = await import("./index.js");
+const pluginModule = await import("./index.internal.js");
 
 function freshContext() {
   return {
